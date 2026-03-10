75ca5cc7c2c07c4dfefc63d6977a4da0ebd1b562
Abhijay chaudhary <abhijaychaudhary@Abhijays-MacBook-Air.local>
2026-03-10 14:44:04 +0530

    fix vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: "client",
+
   plugins: [react()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src")
+      "@": path.resolve(__dirname, "client/src")
     }
   },
-  root: "client",
+
   build: {
     outDir: "../dist/public",
     emptyOutDir: true
